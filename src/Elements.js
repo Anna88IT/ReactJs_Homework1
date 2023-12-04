2598c4ba0dc7168deb39e1bcfe61833f05f959e9
Anna88IT <anna.khach1988@gmail.com>
2023-12-04 10:37:26 +0400

    last change

--- src/Elements.js
+++ src/Elements.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import "./Acordion.css";
 
-export const Elements = ({title, text,  }) => {
+export const Elements = ({title, text}) => {
 
     const [val, setVal] = useState(true);
 
